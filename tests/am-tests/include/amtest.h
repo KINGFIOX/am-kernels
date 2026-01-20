8a618dce3f5b33a02b89c165c83e246d52e632e8
wangfiox <wangfiox@gmail.com>
2026-01-20 08:10:21 +0800

    删掉不用的平台

--- tests/am-tests/include/amtest.h
+++ tests/am-tests/include/amtest.h
@@ -8,7 +8,6 @@
 #define IOE ({ ioe_init();  })
 #define CTE(h) ({ Context *h(Event, Context *); cte_init(h); })
 #define VME(f1, f2) ({ void *f1(int); void f2(void *); vme_init(f1, f2); })
-#define MPE ({ mpe_init(entry); })
 
 extern void (*entry)();
 
